fd555dba007d5dfa2def8db6ddad45880dc203bb
Patrick <patrick.silvestre13@hotmail.com>
2021-12-30 20:52:39 -0300

    Escolhendo fotos do anuncio

--- app/src/main/java/com/example/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olx/activity/CadastrarAnuncioActivity.java
@@ -1,31 +1,41 @@
 package com.example.olx.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.olx.R;
 import com.example.olx.helper.Permissoes;
 import com.santalu.maskara.widget.MaskEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +55,64 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK){
+
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configura imagem no ImagemView
+            if (requestCode == 1){
+                imagem1.setImageURI(imagemSelecionada);
+            }else if (requestCode == 2){
+                imagem2.setImageURI(imagemSelecionada);
+            }else if (requestCode == 3){
+                imagem3.setImageURI(imagemSelecionada);
+            }
+
+            listaFotosRecuperadas.add(caminhoImagem);
+
+        }
+
+    }
+
     private void inicializarComponentes(){
         campoTitulo = findViewById(R.id.editTitulo);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editValor);
         campoTelefone = findViewById(R.id.editTelefone);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
 
         //Configura a localidade para pt portugues
         Locale locale = new Locale("pt", "BR");
