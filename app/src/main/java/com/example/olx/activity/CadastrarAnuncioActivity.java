8feac97f8d2d25b87b8367475d7e8aa94e4df603
Patrick <patrick.silvestre13@hotmail.com>
2021-12-29 21:48:21 -0300

    Utilizando mascaras para telefone e moeda

--- app/src/main/java/com/example/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olx/activity/CadastrarAnuncioActivity.java
@@ -3,14 +3,46 @@ package com.example.olx.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.olx.R;
+import com.santalu.maskara.widget.MaskEditText;
+
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
+
     }
+
+    public void salvarAnuncio(View view){
+
+
+
+    }
+
+    private void inicializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+
+        //Configura a localidade para pt portugues
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale(locale);
+
+    }
+
 }
