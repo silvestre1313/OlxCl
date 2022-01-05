b5c2a54346eba4ab29d36494a0dd0032ab3782e5
Patrick <patrick.silvestre13@hotmail.com>
2022-01-04 21:44:53 -0300

    Listando meus anuncios

--- app/src/main/java/com/example/olx/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/example/olx/activity/MeusAnunciosActivity.java
@@ -3,43 +3,106 @@ package com.example.olx.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.olx.adapter.AdapterAnuncios;
 import com.example.olx.databinding.ActivityMeusAnunciosBinding;
+import com.example.olx.helper.ConfiguracaoFirebase;
+import com.example.olx.model.Anuncio;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.olx.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMeusAnunciosBinding binding;
+   // private ActivityMeusAnunciosBinding binding;
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
     private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_anuncios);
 
-        binding = ActivityMeusAnunciosBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        fab = findViewById(R.id.fab);
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child(ConfiguracaoFirebase.getIdUsuario());
 
-        setSupportActionBar(binding.toolbar);
+        inicializarComponentes();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        fab.setOnClickListener(view -> {
-            startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class));
-        });
+        fab.setOnClickListener(v-> startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class)));
+
+        // binding = ActivityMeusAnunciosBinding.inflate(getLayoutInflater());
+        // setContentView(binding.getRoot());
+        // setSupportActionBar(binding.toolbar);
+
+
+        //Configurar recyclerView
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        //Recupera anuncios para o usuario
+        recuperarAnuncios();
+
     }
 
+    public void recuperarAnuncios(){
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                anuncios.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    anuncios.add(ds.getValue(Anuncio.class));
+                }
+
+                Collections.reverse(anuncios);
+                adapterAnuncios.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    public void inicializarComponentes(){
+
+        fab = findViewById(R.id.fab);
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
+
+    }
 }
